2b5e9ba3901899c80b1f76e12c09063e472aaf6c
Martina Maggio <maggio.martina@gmail.com>
2021-12-13 09:53:46 +0100

    day 13 solution

---
+++ 13/p2.py
@@ -0,0 +1,44 @@
+# https://adventofcode.com/2021/
+# day 13, puzzle 2
+
+import numpy as np
+
+
+points = list()
+instructions = list()
+
+# parsing input
+with open('input.txt') as f:
+  for line in f:
+    if line == "\n": continue
+    elif line.startswith("fold"):
+      direction, coordinate = line.rstrip().split(" ")[2].split("=")
+      instructions.append([direction, int(coordinate)])
+    else:
+      points.append([int(v) for v in line.rstrip().split(",")])
+
+# build initial paper
+paper = np.zeros((max([x[1] for x in points])+1, max([x[0] for x in points])+1), dtype=int)
+for x in points:
+  paper[x[1], x[0]] = 1
+
+for i in instructions:
+  direction, coordinate = i[0], i[1]
+  xmax, ymax = paper.shape
+
+  if direction == 'y':
+    top, bottom = paper[0:coordinate, 0:ymax], paper[coordinate+1:xmax, 0:ymax]
+    new_bottom = np.zeros(top.shape)
+    new_bottom[top.shape[0]-bottom.shape[0]:top.shape[0], 0:ymax] = np.flip(bottom, axis=0)
+    paper = top + new_bottom
+
+  if direction == 'x':
+    left, right = paper[0:xmax, 0:coordinate], paper[0:xmax, coordinate+1:ymax]
+    new_right = np.zeros(left.shape)
+    new_right[0:xmax, left.shape[1]-right.shape[1]:left.shape[1]] = np.flip(right, axis=1)
+    paper = left + new_right
+
+for x in range(paper.shape[0]):
+  for y in range(paper.shape[1]):
+    print("x", end="") if paper[x, y] > 0 else print(" ", end="")
+  print("")
