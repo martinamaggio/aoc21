638b2cdb6188f625d37f166d255f4662c2f342bf
Martina Maggio <maggio.martina@gmail.com>
2021-12-19 23:40:52 +0100

    day 18 solution

---
+++ 18/p2.py
@@ -0,0 +1,149 @@
+import math
+import itertools
+
+with open("input.txt") as f:
+  lines = [line.rstrip() for line in f]
+
+
+class Pair:
+
+  def __init__(self, l, r):
+    self.left = l
+    self.right = r
+    self.parent = None
+    self.set_children()
+    self.isright = False
+    self.isleft = False
+
+  def __repr__(self):
+    return "<" + str(self.left) + "," + str(self.right) + ">"
+
+  def set_children(self):
+    self.left.parent = self
+    self.right.parent = self
+    self.left.isleft, self.left.isright = True, False
+    self.right.isleft, self.right.isright = False, True
+
+  def rewrite(self, side, element):
+    if side == 'r':
+      self.right = element
+      self.right.parent, self.right.isleft, self.right.isright = self, False, True
+      self.right.set_children()
+    if side == 'l':
+      self.left = element
+      self.left.parent, self.left.isleft, self.left.isright = self, True, False
+      self.left.set_children()
+
+  def get_leftof(self):
+    if self.parent is None: return None
+    elif self.isright: return self.parent.left
+    else: return self.parent.get_leftof()
+
+  def get_rightof(self):
+    if self.parent is None: return None
+    elif self.isleft: return self.parent.right
+    else: return self.parent.get_rightof()
+
+  def magnitude(self):
+    return 3 * self.left.magnitude() + 2 * self.right.magnitude()
+
+  def split(self, caller=None, side='x'):
+    return self.left.split(self, 'l') or self.right.split(self, 'r')
+
+  def add_left(self, v): self.left.add_left(v)
+  def add_right(self, v): self.right.add_right(v)
+
+  def explode(self, depth=0):
+
+    if depth < 3:
+      return self.left.explode(depth+1) or self.right.explode(depth+1)
+
+    if depth >= 3:  # if I have a pair I'll have to explode
+
+      if isinstance(self.left, Pair):
+        l, r = self.left.left.v, self.left.right.v  # save values
+        self.left = Number(0, self, True, False)  # overwriting with a left number zero
+        self.right.add_left(r)
+        if self.get_leftof() is not None:
+          self.get_leftof().add_right(l)
+        return True
+
+      elif isinstance(self.right, Pair):
+        l, r = self.right.left.v, self.right.right.v
+        self.right = Number(0, self, False, True)
+        self.left.add_right(l)
+        if self.get_rightof() is not None:
+          self.get_rightof().add_left(r)
+        return True
+
+      else:
+        return False
+
+
+class Number:
+  def __init__(self, v, parent=None, isleft=False, isright=False):
+    self.v = v
+    self.parent = parent
+    self.isright = isright
+    self.isleft = isleft
+
+  def __repr__(self):
+    return str(self.v)
+
+  def split(self, caller, side):
+    if self.v < 10: return False
+    else:
+      caller.rewrite(side, Pair(Number(math.floor(self.v / 2)), Number(math.ceil(self.v / 2))))
+      return True
+
+  def add_left(self, v): self.v += v
+  def add_right(self, v): self.v += v
+  def magnitude(self): return self.v
+  def explode(self, depth=0): pass
+
+
+def parse_number(n):
+  if isinstance(n, list):
+    return Pair(parse_number(n[0]), parse_number(n[1]))
+  else:
+    return Number(n)
+
+
+def reduce(x):
+  while True:
+    if x.explode():
+      pass
+    elif x.split():
+      pass
+    else: break  # I tried both and neither worked, I am done
+
+
+def clone(x):
+  if isinstance(x, Pair):
+    return Pair(clone(x.left), clone(x.right))
+  return Number(x.v)
+
+
+numbers = []
+for ll in lines:
+  numbers.append(parse_number(eval(ll)))
+
+answer = 0
+values = range(len(numbers))
+cycle = itertools.combinations(values, 2)
+
+for n1, n2 in cycle:
+
+  sum1 = Pair(clone(numbers[n1]), clone(numbers[n2]))
+  reduce(sum1)
+  mag1 = sum1.magnitude()
+  if mag1 > answer:
+    answer = mag1
+
+  sum2 = Pair(clone(numbers[n2]), clone(numbers[n1]))
+  reduce(sum2)
+  mag2 = sum2.magnitude()
+  if mag2 > answer:
+    answer = mag2
+
+print("Answer: %d" % answer)
